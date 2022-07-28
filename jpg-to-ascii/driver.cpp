49dc95078bf95df0614c4230d264db366c2ffa9a
Drew Daly <ad120136.drew@gmail.com>
2022-07-27 20:11:01 -0500

    Got directories figured out so we can load images. PUT IMAGES IN PROJECT DIRECTORY

--- jpg-to-ascii/driver.cpp
+++ jpg-to-ascii/driver.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include "SFML/Graphics.hpp"
 #include "functions.hpp"
-#define cimg_use_magick
 
 
 int main()
 {
+	sf::Image img;
+	img.loadFromFile("blindman's meal.jpg");
+	std::cout << (char)img.getPixel(49, 49).g << std::endl;
+	system("pause>0");
 	return 0;
 }
 
--- jpg-to-ascii/functions.hpp
+++ jpg-to-ascii/functions.hpp
@@ -5,7 +5,7 @@
 
 #include <string>
 
-#include <SFML/Graphics.hpp>
+#include "SFML/Graphics.hpp"
 
 using namespace sf;
 
